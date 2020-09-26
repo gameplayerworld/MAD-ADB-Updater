f4b1d760438262f891bacb40209e9885df7965c9
gameplayerworld <gameplayerworld@live.de>
2020-09-26 11:37:59 +0200

    Pogo download from MADmin

--- Pogo-Updater.sh
+++ Pogo-Updater.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
+
+if [ -z "$1" ] && [ -z "$2" ]; then
+    echo "Commandline options are missing! Usage: ./Pogo-Updater.sh user:pass localhost:9000"
+    exit 1
+	
+fi	
 echo "Start"
 echo "Delete old Files"
 ##delete
@@ -8,25 +14,11 @@ rm icon.png
 rm .info.json
 sleep 2
 
-##Get apkm
-echo "Fetch latest apkm"
-curl -s -k -A "Mozilla/5.0 (Windows; U; Windows NT 5.1; de; rv:1.9.2.3) Gecko/20100401 Firefox/3.6.3" -L -o ./pogo.apkm "https://www.apkmirror.com/wp-content/themes/APKMirror/download.php?id=$(curl -s -k -A "Mozilla/5.0 (Windows; U; Windows NT 5.1; de; rv:1.9.2.3) Gecko/20100401 Firefox/3.6.3" -L "$(curl -s -k -A "Mozilla/5.0 (Windows; U; Windows NT 5.1; de; rv:1.9.2.3) Gecko/20100401 Firefox/3.6.3" -L 'https://www.apkmirror.com/apk/niantic-inc/pokemon-go/variant-%7B%22arches_slug%22%3A%5B%22armeabi-v7a%22%5D%7D/'|  grep -o -P "(?<=href=\").*android-apk-download"|head -n1|eval 'stdin=$(cat); echo "https://apkmirror.com$stdin"')"|grep data-postid|head -n1|awk -F'"' '{print $14}')"
+##Get PoGo from Madmin
+echo "Fetch latest apk"
+curl -o pogo.zip -u $1 http://$2/mad_apk/pogo/arm64_v8a/download 
 
-##Unapkm exist
-echo "unapkm File check"
-FILE=unapkm.jar
-if test -f "$FILE"; then
-echo "$FILE exist"
-else
-echo "unapkm not found"
-wget https://github.com/souramoo/unapkm/releases/download/v1.2/unapkm.jar
-fi
-echo "Install APK"
-
-#unzip
-echo "unzip"
-java -jar unapkm.jar ./pogo.apkm pogo.zip
-unzip pogo.zip
+unzip pogo.zip && rm pogo.zip
 
 ##Install
 echo "Install APK"
@@ -39,7 +31,7 @@ mapfile -t devices < devices.txt
 for i in "${devices[@]}"; do
 adb connect  $i:5555
 echo "APK Install on $i"
-adb -s $i:5555 install-multiple -r base.apk split_config.xxxhdpi.apk split_config.armeabi_v7a.apk
+adb -s $i:5555 install-multiple -r base.apk config.arm64_v8a.apk
 adb -s $i:5555 reboot
 adb disconnect $i:5555
 echo "Device $i done."
